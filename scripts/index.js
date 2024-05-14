ba7ea7edbbd3c742ca0452f00cf294477c0c204c
Z.Chabot <chabzach95@gmail.com>
2024-05-14 11:39:53 -0400

    New update for my project

---
+++ scripts/index.js
@@ -0,0 +1,34 @@
+const initialCards =[
+
+const cardData1 {
+  name: "Yosemite Valley";
+  link: "images/yosemite.jpg";
+}
+
+const cardData2 {
+  name: "Lake Louise";
+  link: "images/lake-louise.jpg";
+}
+
+
+const cardData3 {
+  name: "Bald Mountains";
+  link: "images/bald-mountains.jpg";
+}
+
+
+const cardData4 {
+  name: "Latemar";
+  link: "images/latemar.jpg";
+}
+
+const cardData5 {
+  name: "Vanoise National Park";
+  link: "images/vanoise.jpg";
+}
+
+const cardData6 {
+  name: "Lago di Braies";
+  link: "images/lago.jpg";
+}
+];
